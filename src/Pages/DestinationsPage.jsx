8b9963f43aa546483316d064dead9893a3a5ec64
AbdulRahman-Kreit <aleppianeren@gmail.com>
2026-07-09 15:52:05 +0300

    Fixed images paths

--- src/Pages/DestinationsPage.jsx
+++ src/Pages/DestinationsPage.jsx
@@ -4,9 +4,9 @@ import LoadingSpinner from "../Components/LoadingSpinner";
 const Destination = lazy(() => import("../Components/Destination"));
 
 const DestinationsPage = memo(() => {
-    const pageBackground = `bg-[url('../public/assets/destination/background-destination-mobile.jpg')]
-    md:bg-[url('../public/assets/destination/background-destination-tablet.jpg')]
-    lg:bg-[url('../public/assets/destination/background-destination-desktop.jpg')] bg-cover`;
+    const pageBackground = `bg-[url('../src/assets/destination/background-destination-mobile.jpg')]
+    md:bg-[url('../src/assets/destination/background-destination-tablet.jpg')]
+    lg:bg-[url('../src/assets/destination/background-destination-desktop.jpg')] bg-cover`;
 
     return (
         <section className={`${pageBackground} w-full min-h-screen top-0 left-0 pt-40 
